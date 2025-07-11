cd86c5ad900eeab3d9f80d231a0233c4d68e1bf6
trainghost <trainghost@Naver.com>
2025-07-11 10:50:11 +0900

    Update app.py

--- app.py
+++ app.py
@@ -76,4 +76,4 @@ def generate_match():
     return render_template("matches.html", courts=courts)
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run(host="0.0.0.0", port=5000, debug=True)
