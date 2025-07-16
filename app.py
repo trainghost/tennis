65be625c3cbf0af77f86e8e8cd2cbe761907d326
trainghost <trainghost@Naver.com>
2025-07-16 10:21:00 +0900

    Update app.py

--- app.py
+++ app.py
@@ -1,3 +1,12 @@
+from flask import Flask
 import os
-port = int(os.environ.get("PORT", 5000))
-app.run(host="0.0.0.0", port=port)
+
+app = Flask(__name__)
+
+@app.route('/')
+def home():
+    return "Hello from Flask on Render!"
+
+if __name__ == '__main__':
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
