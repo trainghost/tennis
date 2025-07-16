d18aa06591e6326195de1fc283021ffa08b42ce3
trainghost <trainghost@Naver.com>
2025-07-16 10:07:02 +0900

    Update app.py

--- app.py
+++ app.py
@@ -139,4 +139,5 @@ def index():
     return render_template("index.html", match_text=match_text)
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run(host="0.0.0.0", port=int(os.environ.get("PORT", 5000)), debug=True)
+
