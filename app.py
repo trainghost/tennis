6d88e6d779e39d4f75f35db13b99d349ccb7c316
trainghost <trainghost@Naver.com>
2025-07-17 15:55:25 +0900

    Update app.py

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 from flask import Flask, render_template, request, redirect, url_for
 import pandas as pd
 import os
+import random
 
 app = Flask(__name__)
 UPLOAD_FOLDER = 'uploads'
@@ -25,7 +26,7 @@ def upload():
     global members_data
     members_data = df.to_dict(orient='records')
 
-    return redirect(url_for('members'))  # 여기서 members 페이지로 리다이렉트
+    return redirect(url_for('members'))  # 업로드 후 /members로 리다이렉트
 
 @app.route('/members', methods=['GET', 'POST'])
 def members():
@@ -36,14 +37,23 @@ def members():
             member['일퇴'] = 'early_' + str(idx) in request.form
             member['늦참'] = 'late_' + str(idx) in request.form
 
-        # 여기에 매칭 로직을 추가하면 됩니다
-        participants_1 = [m for m in members_data if m.get('참가')]  # 예시로 참가한 회원들만 추출
-        participants_2 = [m for m in members_data if not m.get('참가')]  # 참가하지 않은 회원들
+        # 매칭 1: 참가 체크된 사람 중 일퇴 체크된 사람 우선 뽑기
+        participants_early = [m for m in members_data if m.get('참가') and m.get('일퇴')]
+        
+        # 만약 12명이 안되면 늦참 체크 안 된 사람 중에서 랜덤으로 뽑기
+        if len(participants_early) < 12:
+            remaining_count = 12 - len(participants_early)
+            participants_late = [m for m in members_data if m.get('참가') and not m.get('늦참')]
+            additional_participants = random.sample(participants_late, remaining_count)
+            participants_early.extend(additional_participants)
+        
+        # 매칭 1에 해당하는 12명을 선정
+        participants_1 = participants_early[:12]
 
-        return render_template('members.html', members=members_data, participants_1=participants_1, participants_2=participants_2)
+        return render_template('members.html', members=members_data, participants_1=participants_1)
     
     # GET 요청일 경우 (페이지를 처음 로드한 경우)
-    return render_template('members.html', members=members_data, participants_1=[], participants_2=[])
+    return render_template('members.html', members=members_data, participants_1=[])
 
 if __name__ == "__main__":
     port = int(os.environ.get("PORT", 5000))
